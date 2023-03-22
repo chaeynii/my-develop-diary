afa231806b6308ff0c0c13ea95aebf6a89d02934
chaeyni <dlco9292@naver.com>
2023-03-23 01:16:58 +0900

    Som - commit 3/23

--- mainjs.js
+++ mainjs.js
@@ -8,9 +8,25 @@
     this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
     };
     */
+
 (function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = 'https://web1-jpw39gzev5.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
+    var d = document, s = d.createElement('script');
+    s.src = 'https://web1-jpw39gzev5.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+
+let observer = new IntersectionObserver((e)=>{
+    e.forEach((icon)=>{
+        if (icon.isIntersecting){
+            icon.target.style.opacity = 1;
+        } else{
+            icon.target.style.opacity = 0;
+        }
+    })
+})
+
+var bd = document.getElementsByClassName("bd");
+observer.observe(bd[0]);
+observer.observe(bd[1]);
+observer.observe(bd[2]);
